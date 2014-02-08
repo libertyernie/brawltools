bbd0cd5d043378e1bcba706b316846339b7f1252
libertyernie <libertyernie0@gmail.com>
2014-02-08 09:24:55 -0600

    Code cleanup

--- BrawlLib/System/Windows/Forms/StageModuleConverter.cs
+++ BrawlLib/System/Windows/Forms/StageModuleConverter.cs
@@ -1,5 +1,4 @@
-﻿#define STANDALONE
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Text;
 using System.Collections.Generic;
 using System.IO;
@@ -35,7 +34,56 @@ namespace System.Windows.Forms
         }
         #endregion
 
-        private static List<Stage> stageList = new List<Stage>();
+		private static Stage[] stageList = new Stage[] {
+			new Stage(0, "STGCUSTOM##.pac", "st_custom##.rel"),
+			new Stage(1, "Battlefield", "st_battle.rel"),
+			new Stage(2, "Final Destination", "st_final.rel"),
+			new Stage(3, "Delfino Plaza", "st_dolpic.rel"),
+			new Stage(4, "Luigi's Mansion", "st_mansion.rel"),
+			new Stage(5, "Mushroomy Kingdom", "st_mariopast.rel"),
+			new Stage(6, "Mario Circuit", "st_kart.rel"),
+			new Stage(7, "75 m", "st_donkey.rel"),
+			new Stage(8, "Rumble Falls", "st_jungle.rel"),
+			new Stage(9, "Pirate Ship", "st_pirates.rel"),
+			new Stage(11, "Norfair", "st_norfair.rel"),
+			new Stage(12, "Frigate Orpheon", "st_orpheon.rel"),
+			new Stage(13, "Yoshi's Island (Brawl)", "st_crayon.rel"),
+			new Stage(14, "Halberd", "st_halberd.rel"),
+			new Stage(19, "Lylat Cruise", "st_starfox.rel"),
+			new Stage(20, "Pokemon Stadium 2", "st_stadium.rel"),
+			new Stage(21, "Spear Pillar", "st_tengan.rel"),
+			new Stage(22, "Port Town Aero Dive", "st_fzero.rel"),
+			new Stage(23, "Summit", "st_ice.rel"),
+			new Stage(24, "Flat Zone 2", "st_gw.rel"),
+			new Stage(25, "Castle Siege", "st_emblem.rel"),
+			new Stage(28, "WarioWare Inc.", "st_madein.rel"),
+			new Stage(29, "Distant Planet", "st_earth.rel"),
+			new Stage(30, "Skyworld", "st_palutena.rel"),
+			new Stage(31, "Mario Bros.", "st_famicom.rel"),
+			new Stage(32, "New Pork City", "st_newpork.rel"),
+			new Stage(33, "Smashville", "st_village.rel"),
+			new Stage(34, "Shadow Moses Island", "st_metalgear.rel"),
+			new Stage(35, "Green Hill Zone", "st_greenhill.rel"),
+			new Stage(36, "PictoChat", "st_pictchat.rel"),
+			new Stage(37, "Hanenbow", "st_plankton.rel"),
+			new Stage(38, "ConfigTest", "st_config.rel"),
+			new Stage(41, "Temple", "st_dxshrine.rel"),
+			new Stage(42, "Yoshi's Island (Melee)", "st_dxyorster.rel"),
+			new Stage(43, "Jungle Japes", "st_dxgarden.rel"),
+			new Stage(44, "Onett", "st_dxonett.rel"),
+			new Stage(45, "Green Greens", "st_dxgreens.rel"),
+			new Stage(46, "Pokemon Stadium", "st_dxpstadium.rel"),
+			new Stage(47, "Rainbow Cruise", "st_dxrcruise.rel"),
+			new Stage(48, "Corneria", "st_dxcorneria.rel"),
+			new Stage(49, "Big Blue", "st_dxbigblue.rel"),
+			new Stage(50, "Brinstar", "st_dxzebes.rel"),
+			new Stage(51, "Bridge of Eldin", "st_oldin.rel"),
+			new Stage(52, "Homerun", "st_homerun.rel"),
+			new Stage(53, "Edit", "st_stageedit.rel"),
+			new Stage(54, "Heal", "st_heal.rel"),
+			new Stage(55, "Online Training", "st_otrain.rel"),
+			new Stage(56, "TargetBreak", "st_tbreak.rel"),
+		};
 		private static int[] indicesToIgnore = {
 			2959, // st_croll (PAL)
 			431, // st_onett, st_metalgear
@@ -44,60 +92,8 @@ namespace System.Windows.Forms
 			419, // st_donkey
 			423, // st_halberd, st_jungle, st_mansion
 			};
-		public static ReadOnlyCollection<Stage> StageList { get { return stageList.AsReadOnly(); } }
+		public static ReadOnlyCollection<Stage> StageList { get { return Array.AsReadOnly(stageList); } }
 		public static ReadOnlyCollection<int> IndicesToIgnore { get { return Array.AsReadOnly(indicesToIgnore); } }
-        private Label lblIDValue;
-        private Label lblIDDesc;
-        private Label label3;
-        private ComboBox itemSelection;
-        private Label lblItemDesc;
-        static StageModuleConverter()
-        {
-            // static initializer
-            #region Arrays containing stage data
-            string[] filenames = {"st_custom##.rel",
-				"st_battle.rel", "st_final.rel",
-				"st_dolpic.rel", "st_mansion.rel", "st_mariopast.rel",
-				"st_kart.rel", "st_donkey.rel", "st_jungle.rel",
-				"st_pirates.rel", "st_norfair.rel", "st_orpheon.rel",
-				"st_crayon.rel", "st_halberd.rel", "st_starfox.rel",
-				"st_stadium.rel", "st_tengan.rel", "st_fzero.rel",
-				"st_ice.rel", "st_gw.rel", "st_emblem.rel",
-				"st_madein.rel", "st_earth.rel", "st_palutena.rel",
-				"st_famicom.rel", "st_newpork.rel", "st_village.rel",
-				"st_metalgear.rel", "st_greenhill.rel", "st_pictchat.rel",
-				"st_plankton.rel", "st_config.rel", "st_dxshrine.rel",
-				"st_dxyorster.rel", "st_dxgarden.rel", "st_dxonett.rel",
-				"st_dxgreens.rel", "st_dxpstadium.rel", "st_dxrcruise.rel",
-				"st_dxcorneria.rel", "st_dxbigblue.rel", "st_dxzebes.rel",
-				"st_oldin.rel", "st_homerun.rel", "st_stageedit.rel",
-				"st_heal.rel", "st_otrain.rel", "st_tbreak.rel"};
-            string[] stagenames = {"STGCUSTOM##.pac", "Battlefield",
-				"Final Destination", "Delfino Plaza", "Luigi's Mansion",
-				"Mushroomy Kingdom", "Mario Circuit", "75 m",
-				"Rumble Falls", "Pirate Ship", "Norfair",
-				"Frigate Orpheon", "Yoshi's Island (Brawl)", "Halberd",
-				"Lylat Cruise", "Pokemon Stadium 2", "Spear Pillar",
-				"Port Town Aero Dive", "Summit", "Flat Zone 2",
-				"Castle Siege", "WarioWare Inc.", "Distant Planet",
-				"Skyworld", "Mario Bros.", "New Pork City", "Smashville",
-				"Shadow Moses Island", "Green Hill Zone", "PictoChat",
-				"Hanenbow", "ConfigTest", "Temple",
-				"Yoshi's Island (Melee)", "Jungle Japes", "Onett",
-				"Green Greens", "Pokemon Stadium", "Rainbow Cruise",
-				"Corneria", "Big Blue", "Brinstar", "Bridge of Eldin",
-				"Homerun", "Edit", "Heal", "Online Training",
-				"TargetBreak"};
-            byte[] ids = {0,1,2,3,4,5,6,7,
-				8,9,11,12,13,14,19,20,21,
-				22,23,24,25,28,29,30,31,32,33,
-				34,35,36,37,38,41,
-				42,43,44,45,46,47,48,49,50,51,
-				52,53,54,55,56};
-            #endregion
-            for (int i = 0; i < ids.Length; i++)
-                stageList.Add(new Stage(ids[i], stagenames[i], filenames[i]));
-        }
 
         #region Designer
 
@@ -123,6 +119,11 @@ namespace System.Windows.Forms
         private Label lblSizeDesc;
         private Label lblNameValue;
         private Button btnBrowse;
+		private Label lblIDValue;
+		private Label lblIDDesc;
+		private Label label3;
+		private ComboBox itemSelection;
+		private Label lblItemDesc;
 
         private void InitializeComponent()
         {
@@ -534,7 +535,7 @@ namespace System.Windows.Forms
         {
             InitializeComponent();
 
-            stageSelection.Items.AddRange(stageList.ToArray());
+            stageSelection.Items.AddRange(stageList);
             stageSelection.SelectedIndex = 0;
 
             #region Tooltips
@@ -622,18 +623,13 @@ namespace System.Windows.Forms
             byte currentID = findCurrentID();
             lblIDValue.Text = "0x" + Convert.ToString(currentID, 16);
 
-            IEnumerator<Stage> e = stageList.GetEnumerator();
-            bool found = false;
-            while (e.MoveNext() && !found)
-            {
-                Stage s = e.Current;
-                if (s.ID == currentID)
-                {
-                    found = true;
-                    lblCurrentStage.Text = s.Name;
-                    stageSelection.SelectedItem = s;
-                }
-            }
+			foreach (Stage s in stageList) {
+				if (s.ID == currentID) {
+					lblCurrentStage.Text = s.Name;
+					stageSelection.SelectedItem = s;
+					break;
+				}
+			}
 
             if (lblNameValue.Text.StartsWith("stOnline"))
             {
