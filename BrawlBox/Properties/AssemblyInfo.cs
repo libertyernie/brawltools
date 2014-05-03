e81998977e54d48fe411e4368bd8fefe11a9a350
libertyernie <maybeway36@gmail.com>
2014-05-03 10:53:50 -0500

    Upload new version of BrawlBox

--- BrawlBox/Properties/AssemblyInfo.cs
+++ BrawlBox/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BrawlBox v0.71")]
+[assembly: AssemblyTitle("BrawlBox v0.72")]
 [assembly: AssemblyDescription("This program is provided as-is without any warranty, implied or otherwise." +
 " By using this program, the end user agrees to take full responsibility regarding its proper and lawful use." +
 " The authors/hosts/distributors cannot be held responsible for any damage resulting in the use of this program," +
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.71.*")]
-[assembly: AssemblyFileVersion("0.71.0.0")]
+[assembly: AssemblyVersion("0.72.*")]
+[assembly: AssemblyFileVersion("0.72.0.0")]
