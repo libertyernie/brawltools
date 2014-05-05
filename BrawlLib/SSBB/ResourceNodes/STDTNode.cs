e4ea4912834373a33d7112b8721648c674a5e51d
libertyernie <maybeway36@gmail.com>
2014-05-05 14:38:54 -0500

    Import STDT code from https://code.google.com/p/brawldev/source/browse/trunk/BrawlLib/SSBB/

--- BrawlLib/SSBB/ResourceNodes/STDTNode.cs
+++ BrawlLib/SSBB/ResourceNodes/STDTNode.cs
@@ -1,38 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using BrawlLib.Wii;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using BrawlLib.IO;
+using BrawlLib.SSBBTypes;
 
-namespace BrawlLib.SSBB.ResourceNodes
-{
-    public unsafe class STDTNode : ARCEntryNode
-    {
-		public override ResourceType ResourceType { get { return ResourceType.STDT; } }
-		public Tuple<bfloat, buint>[] Values { get; private set; }
+namespace BrawlLib.SSBB.ResourceNodes {
+	public unsafe class STDTNode : ARCEntryNode {
+		internal STDT* Header { get { return (STDT*)WorkingUncompressed.Address; } }
+		internal int version, unk1, unk2;
+		internal List<float> entries = new List<float>();
 
-        public override bool OnInitialize()
-        {
-			base.OnInitialize();
+		[Category("Stage Trap Data Table")]
+		public int Unk1 { get { return unk1; } set { unk1 = value; SignalPropertyChange(); } }
+		[Category("Stage Trap Data Table")]
+		public int Unk2 { get { return unk2; } set { unk2 = value; SignalPropertyChange(); } }
+		[Category("Stage Trap Data Table")]
+		public List<float> Entries { get { return entries; } set { entries = value; SignalPropertyChange(); } }
 
-			if (_name == null)
-				_name = "STDT";
-
-            byte* floor = (byte*)WorkingUncompressed.Address + 0x14;
-            byte* ceiling = (byte*)WorkingUncompressed.Address + WorkingUncompressed.Length;
-
-			List<Tuple<bfloat, buint>> values = new List<Tuple<bfloat, buint>>();
-			for (bfloat* ptr = (bfloat*)floor; ptr != (bfloat*)ceiling; ptr++) {
-				values.Add(new Tuple<bfloat, buint>(*ptr, *((buint*)ptr)));
+		public override bool OnInitialize() {
+			_name = "STDT";
+			version = Header->_version;
+			unk1 = Header->_unk1;
+			unk2 = Header->_unk2;
+			bfloat* Entries = Header->Entries;
+			for (int i = 0; i * 0x4 + 0x14 < WorkingUncompressed.Length; i++) {
+				entries.Add(*Entries); Entries++;
 			}
-			this.Values = values.ToArray();
-            return false;
-        }
+			return false;
+		}
 
-        public override void OnRebuild(VoidPtr address, int length, bool force)
-        {
-            // Won't implement unless I add editing support
-        }
-		
-        internal static ResourceNode TryParse(DataSource source) { return ((uint*)source.Address)[0] == 0x54445453 ? new STDTNode() : null; }
-    }
+		public override void OnRebuild(VoidPtr address, int length, bool force) {
+			STDT* header = (STDT*)address;
+			header->_tag = STDT.Tag;
+			header->_unk1 = unk1;
+			header->_unk2 = unk2;
+			header->_version = version;
+			header->_entryOffset = 0x14;
+			bfloat* Entry = header->Entries;
+			for (int i = 0; i < entries.Count; i++) { Entry[i] = entries[i]; }
+		}
+
+		public override int OnCalculateSize(bool force) {
+			return 0x14 + entries.Count * 0x4;
+		}
+
+		internal static ResourceNode TryParse(DataSource source) { return ((STDT*)source.Address)->_tag == STDT.Tag ? new STDTNode() : null; }
+
+	}
+	public class STDTData//external data
+	{
+		int address = 0;
+		string valueName = null;
+		string desctription = null;
+		string defaultValue = null;
+		NumberType type = NumberType.Float;
+
+		public int Address { get { return address; } set { address = value; } }
+		public string ValueName { get { return valueName; } set { valueName = value; } }
+		public string Description { get { return desctription; } set { desctription = value; } }
+		public string DefaultValue { get { return defaultValue; } set { defaultValue = value; } }
+		public NumberType Type { get { return type; } set { type = value; } }
+	}
+
+	public enum NumberType : int {
+		Float = 0,
+		Int = 1,
+	}
 }
