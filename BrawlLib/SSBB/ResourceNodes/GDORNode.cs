5add7afe6a48f928d2a804ae443fc9fa2bf126a4
Sammi-Husky <asylum.band66@gmail.com>
2014-05-07 11:01:33 -0700

    Add GDOR (gah why won't it recognize it in BLOC files)

---
+++ BrawlLib/SSBB/ResourceNodes/GDORNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BrawlLib.SSBBTypes;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using BrawlLib.Imaging;
+using BrawlLib.Wii;
+
+namespace BrawlLib.SSBB.ResourceNodes
+{
+    public unsafe class GDORNode : ResourceNode
+    {
+        internal GDOR* Header { get { return (GDOR*)WorkingUncompressed.Address; } }
+        public override ResourceType ResourceType { get { return ResourceType.Unknown; } }
+
+
+        [Category("BLOC Archive")]
+        public int SubFiles { get { return Header->_count; } }
+
+        public override bool OnInitialize()
+        {
+            base.OnInitialize();
+            if (_name == null)
+                _name = "GDOR";
+            return Header->_count > 0;
+        }
+
+        internal static ResourceNode TryParse(DataSource source) { return ((GDOR*)source.Address)->_tag == GDOR.Tag ? new GDORNode() : null; }
+    }
+}
---
+++ BrawlLib/SSBB/Types/GDOR.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace BrawlLib.SSBBTypes
+{
+   [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct GDOR
+    {
+        public const uint Tag = 0x524F4447;
+        public const int Size = 12;
+        public uint _tag;
+        public bint _count;
+
+        public GDOR(int count)
+        {
+            _tag = Tag;
+            _count = count;
+        }
+        private GDOR* Address { get { fixed (GDOR* ptr = &this)return ptr; } }
+    }
+}
