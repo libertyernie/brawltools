a4b61be01801e7f076804270220a4318e34031d3
libertyernie <maybeway36@gmail.com>
2014-05-08 18:03:34 -0500

    add button, but it doesn't do anything yet

--- BrawlLib/System/Windows/Controls/MultipleInterpretationAttributeList.cs
+++ BrawlLib/System/Windows/Controls/MultipleInterpretationAttributeList.cs
@@ -5,13 +5,30 @@ namespace System.Windows.Forms {
 	public class MultipleInterpretationAttributeGrid : AttributeGrid {
 		private ComboBox chooser;
 
-		public MultipleInterpretationAttributeGrid()
-			: base() {
-			chooser = new ComboBox() {
-				Dock = DockStyle.Top,
-			};
-			this.Controls.Add(chooser);
+		public MultipleInterpretationAttributeGrid() : base() {
+			chooser = new ComboBox() { Dock = DockStyle.Fill };
 			chooser.SelectedIndexChanged += chooser_SelectedIndexChanged;
+
+			Button save = new Button() {
+				Dock = DockStyle.Right,
+				Text = "Save"
+			};
+			save.Click += save_Click;
+
+			Panel p = new Panel() {
+				Dock = DockStyle.Top,
+				Size = chooser.PreferredSize
+			};
+			p.Controls.Add(chooser);
+			p.Controls.Add(save);
+			this.Controls.Add(p);
+			foreach (Control c in new Control[] { chooser, save, p }) {
+				c.Margin = new Padding(0);
+			}
+		}
+
+		void save_Click(object sender, EventArgs e) {
+			throw new NotImplementedException();
 		}
 
 		private void chooser_SelectedIndexChanged(object sender, EventArgs e) {
