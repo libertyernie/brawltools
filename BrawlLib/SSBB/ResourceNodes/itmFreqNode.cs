e60634bd26edeef280e498242e871df4811bf662
Sammi-Husky <asylum.band66@gmail.com>
2014-08-29 02:14:33 -0700

    Kinda hackey..but Item Frequency tables now DISPLAY correctly, but is unstable and alters the file without interaction

---
+++ BrawlLib/SSBB/ResourceNodes/itmFreqNode.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BrawlLib.SSBBTypes;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using BrawlLib.Imaging;
+using BrawlLib.Wii;
+using System.Windows.Forms;
+
+namespace BrawlLib.SSBB.ResourceNodes
+{
+    public unsafe class ItmFreqNode : ARCEntryNode
+    {
+        public override ResourceType ResourceType { get { return ResourceType.U8Folder; } }
+        internal ItmFreqHeader* Header { get { return (ItmFreqHeader*)WorkingUncompressed.Address; } }
+
+        public override bool OnInitialize()
+        {
+            base.OnInitialize();
+            return true;
+        }
+        public override void OnPopulate()
+        {
+            ItmFreqTableList* tList = (ItmFreqTableList*)(WorkingUncompressed.Address + Header->_DataLength - 0x08);
+            DataSource source = new DataSource(tList, 0x28);
+            new TableListNode().Initialize(this, source);
+        }
+        //public override int OnCalculateSize(bool force)
+        //{
+        //    int size = ItmFreqHeader.Size;
+        //    foreach (ResourceNode node in Children)
+        //        size += node.CalculateSize(force);
+        //    return size;
+        //}
+        public override void OnRebuild(VoidPtr address, int length, bool force)
+        {
+            BLOC* header = (BLOC*)address;
+            *header = new BLOC(Children.Count);
+            uint offset = (uint)(0x10 + (Children.Count * 4));
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (i > 0) { offset += (uint)(Children[i - 1].CalculateSize(false)); }
+                *(buint*)((VoidPtr)address + 0x10 + i * 4) = offset;
+                _children[i].Rebuild((VoidPtr)address + offset, _children[i].CalculateSize(false), true);
+            }
+        }
+
+        internal static ResourceNode TryParse(DataSource source) { return ((ItmFreqHeader*)source.Address)->_Length == source.Length &&  ((ItmFreqHeader*)source.Address)->_DataTable == 1 ? new ItmFreqNode() : null; }
+    }
+
+    public unsafe class TableListNode : ResourceNode
+    {
+        internal ItmFreqTableList* Header { get { return (ItmFreqTableList*)WorkingUncompressed.Address; } }
+        public override ResourceType ResourceType { get { return ResourceType.U8Folder; } }
+        
+        public override bool OnInitialize()
+        {
+            base.OnInitialize();
+            Name = "Frequency Tables";
+            return true;
+        }
+        public override void OnPopulate()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                ItmFreqOffPair* entry = (ItmFreqOffPair*)WorkingUncompressed.Address + i;
+                if (entry->_offset != 0 && entry->_count != 0)
+                {
+                    DataSource _tEntry = new DataSource(entry, 0x08);
+                    new TableNode().Initialize(this, _tEntry);
+                }
+            }
+        }
+    }
+    public unsafe class TableNode : ResourceNode
+    {
+        internal ItmFreqOffPair* Header { get { return (ItmFreqOffPair*)WorkingUncompressed.Address; } }
+        public override ResourceType ResourceType { get { return ResourceType.U8Folder; } }
+
+        public override bool OnInitialize()
+        {
+            base.OnInitialize();
+            Name = "Table[" + Parent.Children.IndexOf(this) + "]";
+            return true;
+        }
+        public override void OnPopulate()
+        {
+            for (int i = 0; i < Header->_count; i++)
+            {
+                VoidPtr addr = Parent.Parent.WorkingUncompressed.Address;
+                VoidPtr off = Header->_offset + 0x20;
+
+                ItmFreqGroupNode* entry = (ItmFreqGroupNode*)(addr + off + (i*0x14));
+                    DataSource _group = new DataSource(entry, 0x14);
+                    new TableGroupNode().Initialize(this, _group);
+            }
+        }
+    }
+
+    public unsafe class TableGroupNode : ResourceNode
+    {
+        internal ItmFreqGroupNode* Header { get { return (ItmFreqGroupNode*)WorkingUncompressed.Address; } }
+        public override ResourceType ResourceType { get { return ResourceType.U8Folder; } }
+
+        public override bool OnInitialize()
+        {
+            base.OnInitialize();
+            Name = "Group["+Parent.Children.IndexOf(this)+"]";
+            return true;
+        }
+        public override void OnPopulate()
+        {
+            for (int i = 0; i < Header->_entryCount; i++)
+            {
+                VoidPtr addr = Parent.Parent.Parent.WorkingUncompressed.Address;
+
+                ItmFreqEntry* Entry = (ItmFreqEntry*)(addr + Header->_entryOffset + 0x20 + (i*0x10));
+                DataSource source = new DataSource(Entry, 0x10);
+                new ItmFreq().Initialize(this, source);
+            }
+        }
+    }
+    public unsafe class ItmFreq : ResourceNode
+    {
+        internal ItmFreqEntry* Header { get { return (ItmFreqEntry*)WorkingUncompressed.Address; } }
+        public override ResourceType ResourceType { get { return ResourceType.Unknown; } }
+
+        internal int _id;
+        [DisplayName("Item ID")]
+        [Category("Item")]
+        public int ID { get { return _id; } set { _id = value; SignalPropertyChange(); } }
+
+        internal int _subID;
+        [DisplayName("Sub Item ID")]
+        [Category("Item")]
+        public int SubID { get { return _subID; } set { _subID = value; SignalPropertyChange(); } }
+
+        internal float _frequency;
+        [DisplayName("Frequency")]
+        [Category("Item Frequency")]
+        public float Frequency { get { return _frequency; } set { _frequency = value; SignalPropertyChange(); } }
+
+        internal short _action;
+        [DisplayName("Start Action")]
+        [Category("Item")]
+        public short Action { get { return _action; } set { _action = value; SignalPropertyChange(); } }
+
+        internal short _subaction;
+        [DisplayName("Start Subaction")]
+        [Category("Item")]
+        public short Subaction { get { return _subaction; } set { _subaction = value; SignalPropertyChange(); } }
+
+        public override bool OnInitialize()
+        {
+            base.OnInitialize();
+            _id = Header->_ID;
+            _subID = Header->_subItem;
+            _frequency = Header->_frequency;
+            _action = Header->_action;
+            _subaction = Header->_subaction;
+
+            Name = "Item[" + Parent.Children.IndexOf(this) + "]";
+            return false;
+        }
+        public override void OnPopulate()
+        {
+        }
+    }
+}
--- BrawlLib/SSBB/Types/ItmFreq.cs
+++ BrawlLib/SSBB/Types/ItmFreq.cs
@@ -5,7 +5,6 @@ using System.Runtime.InteropServices;
 
 namespace BrawlLib.SSBBTypes
 {
-    //Alot of this was reused from STPM
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public unsafe struct ItmFreqHeader
     {
@@ -13,36 +12,83 @@ namespace BrawlLib.SSBBTypes
 
         public bint _Length;
         public bint _DataLength;
-        public bint _Offsets;
-        public int _DataTable;
+        public bint _OffCount;
+        public bint _DataTable;
+        public int _pad0;
+        public int _pad1;
+        public int _pad2;
+        public int _pad3;
 
 
         private VoidPtr Address { get { fixed (void* ptr = &this)return ptr; } }
 
-        public ItmFreqHeader(int offsets, int length)
+        public ItmFreqHeader(int offCount, int length)
         {
             _Length = length;
-            _DataLength = length - (offsets*4)+(0x20+0x8)+0xC;
-            _Offsets = offsets;
+            _DataLength = length - (offCount*4)+(0x20+0x8)+0xC;
+            _OffCount = offCount;
             _DataTable = 1;
+            _pad0 = _pad1 =
+            _pad2 = _pad3 = 0;
         }
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public unsafe struct ItmFreqEntry
     {
-        //public bushort _id;
-        //public byte _id2;
-        //public byte _echo;
+        public const int Size = 0x10;
 
-        //public fixed int _values[64];
+        public bint _ID;
+        public bint _subItem;
+        public bfloat _frequency;
+        public short _action;
+        public short _subaction;
 
-        //public ItmFreqEntry(ushort id, byte echo, byte id2)
-        //{
-        //    _id = id;
-        //    _echo = echo;
-        //    _id2 = id2;
-        //}
+        private VoidPtr Address { get { fixed (void* ptr = &this)return ptr; } }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct ItmFreqTableList
+    {
+        public const int Size = 0x28;
+
+        public bint _table1;
+        public bint _t1_count;
+        public bint _table2;
+        public bint _t2_count;
+        public bint _table3;
+        public bint _t3_count;
+        public bint _table4;
+        public bint _t4_count;
+        public bint _table5;
+        public bint _t5_count;
+
+
+        private VoidPtr Address { get { fixed (void* ptr = &this)return ptr; } }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct ItmFreqGroupNode
+    {
+        public const int Size = 0x14;
+
+        public bint _unknown0;
+        public bint _unknown1;
+        public bint _unknown2;
+        public bint _entryOffset;
+        public bint _entryCount;
+        public bint _unknown3;
+
+        private VoidPtr Address { get { fixed (void* ptr = &this)return ptr; } }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct ItmFreqOffPair
+    {
+        public const int Size = 0x08;
+
+        public bint _offset;
+        public bint _count;
 
         private VoidPtr Address { get { fixed (void* ptr = &this)return ptr; } }
     }
