891b0e9f427bd0a35f5a2f19f5b25f940303b5dd
Sammi-Husky <asylum.band66@gmail.com>
2014-10-12 18:40:50 -0700

    Start implementing BGMG + ADSJ fully rebuildable

--- BrawlLib/SSBB/ResourceNodes/Subspace/ADSJNode.cs
+++ BrawlLib/SSBB/ResourceNodes/Subspace/ADSJNode.cs
@@ -32,87 +32,95 @@ namespace BrawlLib.SSBB.ResourceNodes
             base.OnInitialize();
             return Header->_count > 0;
         }
+        public override int OnCalculateSize(bool force)
+        {
+            int size = ADSJ.Size + (Children.Count * 4);
+            foreach (ResourceNode node in Children)
+                size += node.CalculateSize(force);
+            return size;
+        }
+        public override void OnRebuild(VoidPtr address, int length, bool force)
+        {
+            ADSJ* header = (ADSJ*)address;
+            *header = new ADSJ(_children.Count);
+            uint offset = (uint)(0x10 + (Children.Count * 4));
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (i > 0) { offset += (uint)(Children[i - 1].CalculateSize(false)); }
+                *(buint*)((VoidPtr)address + 0x10 + i * 4) = offset;
+                _children[i].Rebuild((VoidPtr)address + offset, _children[i].CalculateSize(false), true);
+            }
+        }
 
-        internal static ResourceNode TryParse(DataSource source) { 
-            return ((ADSJ*)source.Address)->_tag == ADSJ.Tag ? new ADSJNode() : null; }
+        internal static ResourceNode TryParse(DataSource source) { return ((ADSJ*)source.Address)->_tag == ADSJ.Tag ? new ADSJNode() : null; }
     }
 
     public unsafe class ADSJEntryNode : ResourceNode
     {
         internal ADSJEntry* Header { get { return (ADSJEntry*)WorkingUncompressed.Address; } }
         public override ResourceType ResourceType { get { return ResourceType.Unknown; } }
+
+        private string _stageID;
         [Category("Jump Info")]
         [DisplayName("Hexadecimal ID")]
-        public string HexID
-        {
-            get
-            {
-                string s1 = "";
-                for (int i = 0; i < 4; i++)
-                {
-                    int i1 = *(byte*)(Header + 0x08 + i);
-                    if (i1 < 10) { s1 += i1.ToString("x").PadLeft(2, '0'); } else { s1 += i1.ToString("x"); }
-                }
-                return s1;
-            }
-        }
-        
+        public string StageID { get { return _stageID; } set { _stageID = value; SignalPropertyChange(); } }
+
+        private string _sendingID;
         [Category("Jump Info")]
         [DisplayName("Decimal ID")]
-        public string DecID
-        {
-            get
-            {
-                string s1 = "";
-                for (int i = 0; i < 4; i++)
-                {
-                    int i1 = *(byte*)(Header + i);
-                    if (i1 < 10) { s1 += i1.ToString("x").PadLeft(2, '0'); } else { s1 += i1.ToString("x"); }
-                }
-                return s1; 
-            }
-        }
-        
-              
+        public string SendingID { get { return _sendingID; } set { _sendingID = value; SignalPropertyChange(); } }
+
+        private string _jumpBone;
+        [Category("Jump Info")]
+        [DisplayName("Jump Bone?")]
+        public string JumpBone { get { return _jumpBone; } set { _jumpBone = value; Name = value; SignalPropertyChange(); } }
+
+        private byte _flag0;     
         [Category("Jump Flags")]
-        public byte Flag1
-        {
-            get
-            {
-                return *(byte*)(Header+0x04);
-            }
-        }
+        public byte Flag0 { get { return _flag0; } set { _flag0 = value; SignalPropertyChange(); } }
+
+        private byte _flag1;
         [Category("Jump Flags")]
-        public byte Flag2
-        {
-            get
-            {
-                return *(byte*)(Header + 0x05);
-            }
-        }
+        public byte Flag1 { get { return _flag1; } set { _flag1 = value; SignalPropertyChange(); } }
+
+        private byte _flag2;
         [Category("Jump Flags")]
-        public byte Flag3
-        {
-            get
-            {
-                return *(byte*)(Header + 0x06);
-            }
-        }
+        public byte Flag2 { get { return _flag2; } set { _flag2 = value; SignalPropertyChange(); } }
+
+        private byte _flag3;
         [Category("Jump Flags")]
-        public byte Flag4
-        {
-            get
-            {
-                return *(byte*)(Header + 0x07);
-            }
-        }
+        public byte Flag3 { get { return _flag3; } set { _flag3 = value; SignalPropertyChange(); } }
 
         public override bool OnInitialize()
         {
             base.OnInitialize();
+            _stageID = Header->StageID;
+            _sendingID = Header->SendStage;
+            _jumpBone = Header->Name;
+            _flag0 = Header->_unk0;
+            _flag1 = Header->_unk1;
+            _flag2 = Header->_unk2;
+            _flag3 = Header->_unk3;
+
             if (_name == null)
-                _name = new String((sbyte*)(Header + 0x0C));
+                _name = JumpBone;
             return false;
+        }
+        public override int OnCalculateSize(bool force)
+        {
+            return ADSJEntry.Size;
+        }
+        public override void OnRebuild(VoidPtr address, int length, bool force)
+        {
+            ADSJEntry* header = (ADSJEntry*)address;
+            *header = new ADSJEntry();
+            header->_unk0 = _flag0;
+            header->_unk1 = _flag1;
+            header->_unk2 = _flag2;
+            header->_unk3 = _flag3;
+            header->Name = _jumpBone;
+            header->StageID = _stageID;
+            header->SendStage = _sendingID;
         }
     }
 }
