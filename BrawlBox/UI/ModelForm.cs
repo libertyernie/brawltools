99bbf59aee5808c5d3c357f1f1a13d9cb6c5a9a2
BlackJax96 <blackjax0@gmail.com>
2014-10-14 20:05:23 -0700

    Don't want to rebuild the model every time the model editor is opened!

--- BrawlBox/UI/ModelForm.cs
+++ BrawlBox/UI/ModelForm.cs
@@ -70,7 +70,6 @@ namespace BrawlBox
         public DialogResult ShowDialog(MDL0Node model) { return ShowDialog(null, model); }
         public DialogResult ShowDialog(IWin32Window owner, MDL0Node model)
         {
-            model.Rebuild();
             _models.Add(model);
             try { return base.ShowDialog(owner); }
             finally { _models = null; }
