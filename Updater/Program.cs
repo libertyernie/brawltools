76784d88402898e6e7f03df4833108b3043c6c45
Sammi-Husky <asylum.band66@gmail.com>
2014-12-04 12:08:56 -0800

    Commenting. Just in case people want to modify this later

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -27,23 +27,33 @@ namespace Updater
             using (Ping s = new Ping())
                 Console.WriteLine(s.Send("www.github.com").Status);
 
+            // Initiate the github client.
             GitHubClient github = new GitHubClient(new Octokit.ProductHeaderValue("Brawltools"));
 
+            // get repo, Release, and release assets
             Repository repo = await github.Repository.Get("libertyernie", "brawltools");
             Release release = (await github.Release.GetAll(repo.Owner.Login, repo.Name))[0];
             ReleaseAsset Asset = (await github.Release.GetAssets("libertyernie", repo.Name, release.Id))[0];
 
+            // Check if we were passed in the overwrite paramter, and if not create a new folder to extract in.
             if (!Overwrite)
             {
-                Directory.CreateDirectory(apppath + "/"+release.TagName);
+                Directory.CreateDirectory(apppath + "/" + release.TagName);
                 apppath += "/" + release.TagName;
             }
 
             using (WebClient client = new WebClient())
             {
+                // Add the user agent header, otherwise we will get access denied.
                 client.Headers.Add("User-Agent: Other");
+
+                // Base url to search for in the release asset stream
                 string baseUrl = "https://github.com/libertyernie/brawltools/releases/download/";
+
+                // Full asset streamed into a single string
                 string html = client.DownloadString(Asset.Url);
+
+                // The browser download link to the self extracting archive, hosted on github
                 string URL = html.Substring(html.IndexOf(baseUrl)).TrimEnd(new char[] { '}', '"' });
 
                 Console.WriteLine("\nDownloading");
@@ -54,13 +64,14 @@ namespace Updater
                 Console.WriteLine("Starting install");
 
 
-                Process update = Process.Start(apppath + "/Update.exe","-d\"" + apppath+"\"");
+                Process update = Process.Start(apppath + "/Update.exe", "-d\"" + apppath + "\"");
             }
         }
 
 
         static void Main(string[] args)
         {
+            // -r is the overwrite switch.
             if (args.Length > 0 && args[0] == "-r")
             {
                 Task t = UpdateCheck(true);
