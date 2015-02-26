92ad83147020c1cce4ef5d1ea748600b577fd309
BlackJax96 <blackjax0@gmail.com>
2015-02-25 22:34:48 -0800

    Update version

--- BrawlLib/Properties/AssemblyInfo.cs
+++ BrawlLib/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BrawlLib v0.22")]
+[assembly: AssemblyTitle("BrawlLib v0.22b")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.22.*")]
-[assembly: AssemblyFileVersion("0.22.0.2")]
+[assembly: AssemblyFileVersion("0.22.1.0")]
