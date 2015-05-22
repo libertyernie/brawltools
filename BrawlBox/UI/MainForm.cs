059139563927f8d16dd93a2d57191b202ec1d4c9
BlackJax96 <blackjax0@gmail.com>
2015-05-21 17:38:17 -0700

    Update changelog, version id

--- BrawlBox/UI/MainForm.cs
+++ BrawlBox/UI/MainForm.cs
@@ -100,7 +100,7 @@ namespace BrawlBox
                 }
 
                 //Make sure this matches the tag name of the release on github exactly
-                string version = "v0.76_h1";
+                string version = "v0.76_h2";
 
                 var github = new GitHubClient(new Octokit.ProductHeaderValue("Brawltools"));
                 IReadOnlyList<Release> release = null;
