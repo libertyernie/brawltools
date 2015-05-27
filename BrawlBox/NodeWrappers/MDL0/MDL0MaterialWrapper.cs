93bdbef2582b96e749368ff7ea80f3ce1aa3343f
BlackJax96 <blackjax0@gmail.com>
2015-05-26 21:12:17 -0700

    Allow exporting GLSL shader code to text files

--- BrawlBox/NodeWrappers/MDL0/MDL0MaterialWrapper.cs
+++ BrawlBox/NodeWrappers/MDL0/MDL0MaterialWrapper.cs
@@ -5,6 +5,7 @@ using BrawlLib;
 using System.ComponentModel;
 using BrawlLib.IO;
 using BrawlLib.SSBBTypes;
+using BrawlLib.Wii.Graphics;
 
 namespace BrawlBox.NodeWrappers
 {
@@ -26,10 +27,13 @@ namespace BrawlBox.NodeWrappers
             _menu.Items.Add(new ToolStripSeparator());
             _menu.Items.Add(new ToolStripMenuItem("Add New Reference", null, CreateAction, Keys.Control | Keys.Alt | Keys.N));
             _menu.Items.Add(new ToolStripMenuItem("&Delete", null, DeleteAction, Keys.Control | Keys.Delete));
+            _menu.Items.Add(new ToolStripSeparator());
+            _menu.Items.Add(new ToolStripMenuItem("Export GLSL Shader", null, ExportShaderAction));
             _menu.Opening += MenuOpening;
             _menu.Closing += MenuClosing;
         }
         protected static void CreateAction(object sender, EventArgs e) { GetInstance<MDL0MaterialWrapper>().CreateRef(); }
+        protected static void ExportShaderAction(object sender, EventArgs e) { GetInstance<MDL0MaterialWrapper>().ExportShader(); }
         private static void MenuClosing(object sender, ToolStripDropDownClosingEventArgs e)
         {
             _menu.Items[4].Enabled = _menu.Items[5].Enabled = _menu.Items[7].Enabled = true;
@@ -56,6 +60,35 @@ namespace BrawlBox.NodeWrappers
                 Nodes[Nodes.Count - 1].EnsureVisible();
                 //TreeView.SelectedNode = Nodes[Nodes.Count - 1];
             }
+        }
+        private void ExportShader()
+        {
+            MDL0MaterialNode mat = _resource as MDL0MaterialNode;
+
+            ShaderGenerator.Set(mat);
+
+            ShaderGenerator.PixelLighting = MessageBox.Show(MainForm.Instance, "Use per-pixel lighting?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
+
+            SaveFileDialog s = new SaveFileDialog();
+
+            s.Filter = "Text File (*.txt)|*.txt";
+            s.FileName = _resource.Name + " vertex shader";
+            s.Title = "Choose a place to save the vertex shader";
+            if (s.ShowDialog() == DialogResult.OK)
+                System.IO.File.WriteAllText(s.FileName, ShaderGenerator.GenVertexShader().Replace("\n", Environment.NewLine));
+            
+            s.Filter = "Text File (*.txt)|*.txt";
+            s.FileName = _resource.Name + " fragment shader";
+            s.Title = "Choose a place to save the fragment shader";
+            if (s.ShowDialog() == DialogResult.OK)
+            {
+                string m = ShaderGenerator.GenMaterialFragShader();
+                string[] t = ShaderGenerator.GenTEVFragShader();
+                System.IO.File.WriteAllText(s.FileName, ShaderGenerator.CombineFragShader(m, t, mat.ActiveShaderStages).Replace("\n", Environment.NewLine));
+            }
+
+            ShaderGenerator.Clear();
+            ShaderGenerator._pixelLightingChanged = false;
         }
         #endregion
 
