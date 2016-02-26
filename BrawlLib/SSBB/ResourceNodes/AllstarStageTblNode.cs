067b7565999e31d2b3e89ad73f37cf7b78aa3828
libertyernie <maybeway36@gmail.com>
2016-02-26 14:21:16 -0600

    Add all non-padding fields from AllstarStageTbl and children

--- BrawlLib/SSBB/ResourceNodes/AllstarStageTblNode.cs
+++ BrawlLib/SSBB/ResourceNodes/AllstarStageTblNode.cs
@@ -9,44 +9,127 @@ using System.Text;
 
 namespace BrawlLib.SSBB.ResourceNodes
 {
+    /// <summary>
+    /// A class that wraps the data represented by the AllstarDifficultyData structure in a class with properties, so it can be modified by PropertyGrid.
+    /// </summary>
+    public unsafe class AllstarDifficultyDataWrapper
+    {
+        private ResourceNode parent;
+        private AllstarDifficultyData data;
+
+        public byte Unknown00 { get { return data._unknown00; } set { data._unknown00 = value; parent.SignalPropertyChange(); } }
+        public byte Unknown01 { get { return data._unknown01; } set { data._unknown01 = value; parent.SignalPropertyChange(); } }
+        public byte Unknown02 { get { return data._unknown02; } set { data._unknown02 = value; parent.SignalPropertyChange(); } }
+        public byte Unknown03 { get { return data._unknown03; } set { data._unknown03 = value; parent.SignalPropertyChange(); } }
+
+        public short OffenseRatio { get { return data._offenseRatio; } set { data._offenseRatio = value; parent.SignalPropertyChange(); } }
+        public short DefenseRatio { get { return data._defenseRatio; } set { data._defenseRatio = value; parent.SignalPropertyChange(); } }
+
+        public byte Unknown08 { get { return data._unknown08; } set { data._unknown08 = value; parent.SignalPropertyChange(); } }
+        public byte Color { get { return data._color; } set { data._color = value; parent.SignalPropertyChange(); } }
+        public byte Stock { get { return data._stock; } set { data._stock = value; parent.SignalPropertyChange(); } }
+        public byte Unknown0b { get { return data._unknown0b; } set { data._unknown0b = value; parent.SignalPropertyChange(); } }
+
+        public bshort Unknown0c { get { return data._unknown0c; } set { data._unknown0c = value; parent.SignalPropertyChange(); } }
+
+        public AllstarDifficultyDataWrapper(ResourceNode parent, AllstarDifficultyData data)
+        {
+            this.parent = parent;
+            this.data = data;
+        }
+
+        public static explicit operator AllstarDifficultyData(AllstarDifficultyDataWrapper w)
+        {
+            return w.data;
+        }
+    }
+
+    /// <summary>
+    /// A class that wraps the data represented by the AllstarFighterData structure in a class with properties, so it can be modified by PropertyGrid.
+    /// </summary>
+    public unsafe class AllstarFighterDataWrapper
+    {
+        private ResourceNode parent;
+
+        private byte _fighterID;
+        private float _unknown04;
+
+        [TypeConverter(typeof(DropDownListFighterIDs))]
+        public byte FighterID { get { return _fighterID; } set { _fighterID = value; parent.SignalPropertyChange(); } }
+        public float Unknown04 { get { return _unknown04; } set { _unknown04 = value; parent.SignalPropertyChange(); } }
+
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarDifficultyDataWrapper VeryEasy { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarDifficultyDataWrapper Easy { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarDifficultyDataWrapper Normal { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarDifficultyDataWrapper Hard { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarDifficultyDataWrapper VeryHard { get; set; }
+
+        public AllstarFighterDataWrapper(ResourceNode parent, AllstarFighterData data)
+        {
+            this.parent = parent;
+
+            _fighterID = data._fighterID;
+            _unknown04 = data._unknown04;
+            VeryEasy = new AllstarDifficultyDataWrapper(parent, data._difficulty1);
+            Easy = new AllstarDifficultyDataWrapper(parent, data._difficulty2);
+            Normal = new AllstarDifficultyDataWrapper(parent, data._difficulty3);
+            Hard = new AllstarDifficultyDataWrapper(parent, data._difficulty4);
+            VeryHard = new AllstarDifficultyDataWrapper(parent, data._difficulty5);
+        }
+
+        public static explicit operator AllstarFighterData(AllstarFighterDataWrapper w)
+        {
+            return new AllstarFighterData
+            {
+                _fighterID = w._fighterID,
+                _unknown04 = w._unknown04,
+                _difficulty1 = (AllstarDifficultyData)w.VeryEasy,
+                _difficulty2 = (AllstarDifficultyData)w.Easy,
+                _difficulty3 = (AllstarDifficultyData)w.Normal,
+                _difficulty4 = (AllstarDifficultyData)w.Hard,
+                _difficulty5 = (AllstarDifficultyData)w.VeryHard
+            };
+        }
+
+        public override string ToString()
+        {
+            var fighter = BrawlLib.SSBB.Fighter.Fighters.Where(s => s.ID == FighterID).FirstOrDefault();
+            return "All-Star Fighter Data: 0x" + FighterID.ToString("X2") + (fighter == null ? "" : (" - " + fighter.Name));
+        }
+    }
+
     public unsafe class AllstarStageTblNode : ResourceNode
     {
-        private AllstarStageTbl _data;
+        private int _stage1, _stage2, _stage3, _stage4, _stage5;
 
         public override ResourceType ResourceType { get { return ResourceType.Unknown; } }
 
         [TypeConverter(typeof(DropDownListStageIDs))]
-        public byte Stage1 { get { return (byte)(int)_data._stage1; } set { _data._stage1 = (sbyte)value; SignalPropertyChange(); } }
+        public byte Stage1 { get { return (byte)_stage1; } set { _stage1 = (sbyte)value; SignalPropertyChange(); } }
         [TypeConverter(typeof(DropDownListStageIDs))]
-        public byte Stage2 { get { return (byte)(int)_data._stage2; } set { _data._stage2 = (sbyte)value; SignalPropertyChange(); } }
+        public byte Stage2 { get { return (byte)_stage2; } set { _stage2 = (sbyte)value; SignalPropertyChange(); } }
         [TypeConverter(typeof(DropDownListStageIDs))]
-        public byte Stage3 { get { return (byte)(int)_data._stage3; } set { _data._stage3 = (sbyte)value; SignalPropertyChange(); } }
+        public byte Stage3 { get { return (byte)_stage3; } set { _stage3 = (sbyte)value; SignalPropertyChange(); } }
         [TypeConverter(typeof(DropDownListStageIDs))]
-        public byte Stage4 { get { return (byte)(int)_data._stage4; } set { _data._stage4 = (sbyte)value; SignalPropertyChange(); } }
+        public byte Stage4 { get { return (byte)_stage4; } set { _stage4 = (sbyte)value; SignalPropertyChange(); } }
         [TypeConverter(typeof(DropDownListStageIDs))]
-        public byte Stage5 { get { return (byte)(int)_data._stage5; } set { _data._stage5 = (sbyte)value; SignalPropertyChange(); } }
+        public byte Stage5 { get { return (byte)_stage5; } set { _stage5 = (sbyte)value; SignalPropertyChange(); } }
 
-        [TypeConverter(typeof(DropDownListFighterIDs))]
-        public byte Opponent1 { get { return _data._opponent1._fighterID; } set { _data._opponent1._fighterID = value; SignalPropertyChange(); } }
-        [TypeConverter(typeof(DropDownListFighterIDs))]
-        public byte Opponent2 { get { return _data._opponent2._fighterID; } set { _data._opponent2._fighterID = value; SignalPropertyChange(); } }
-        [TypeConverter(typeof(DropDownListFighterIDs))]
-        public byte Opponent3 { get { return _data._opponent3._fighterID; } set { _data._opponent3._fighterID = value; SignalPropertyChange(); } }
-        [TypeConverter(typeof(DropDownListFighterIDs))]
-        public byte Opponent4 { get { return _data._opponent4._fighterID; } set { _data._opponent4._fighterID = value; SignalPropertyChange(); } }
-        [TypeConverter(typeof(DropDownListFighterIDs))]
-        public byte Opponent5 { get { return _data._opponent5._fighterID; } set { _data._opponent5._fighterID = value; SignalPropertyChange(); } }
-
-        public byte Opponent1VeryEasyUnknown00 { get { return _data._opponent1._difficulty1._unknown00; } }
-        public byte Opponent1VeryEasyUnknown01 { get { return _data._opponent1._difficulty1._unknown01; } }
-        public byte Opponent1VeryEasyUnknown02 { get { return _data._opponent1._difficulty1._unknown02; } }
-        public byte Opponent1VeryEasyUnknown03 { get { return _data._opponent1._difficulty1._unknown03; } }
-        public short Opponent1VeryEasyOffenseRatio { get { return _data._opponent1._difficulty1._offenseRatio; } set { _data._opponent1._difficulty1._offenseRatio = value; SignalPropertyChange(); } }
-        public short Opponent1VeryEasyDefenseRatio { get { return _data._opponent1._difficulty1._defenseRatio; } set { _data._opponent1._difficulty1._defenseRatio = value; SignalPropertyChange(); } }
-        public byte Opponent1VeryEasyUnknown08 { get { return _data._opponent1._difficulty1._unknown08; } }
-        public byte Opponent1VeryEasyColor { get { return _data._opponent1._difficulty1._color; } set { _data._opponent1._difficulty1._color = value; SignalPropertyChange(); } }
-        public byte Opponent1VeryEasyStock { get { return _data._opponent1._difficulty1._stock; } set { _data._opponent1._difficulty1._stock = value; SignalPropertyChange(); } }
-        public byte Opponent1VeryEasyUnknown0b { get { return _data._opponent1._difficulty1._unknown0b; } }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarFighterDataWrapper Opponent1 { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarFighterDataWrapper Opponent2 { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarFighterDataWrapper Opponent3 { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarFighterDataWrapper Opponent4 { get; set; }
+        [Category("Fighters"), TypeConverter(typeof(ExpandableObjectConverter))]
+        public AllstarFighterDataWrapper Opponent5 { get; set; }
 
         public override bool OnInitialize()
         {
@@ -54,7 +137,17 @@ namespace BrawlLib.SSBB.ResourceNodes
 
             // Copy the data from the address
             AllstarStageTbl* dataPtr = (AllstarStageTbl*)WorkingUncompressed.Address;
-            _data = *dataPtr;
+            _stage1 = dataPtr->_stage1;
+            _stage2 = dataPtr->_stage2;
+            _stage3 = dataPtr->_stage3;
+            _stage4 = dataPtr->_stage4;
+            _stage5 = dataPtr->_stage5;
+
+            Opponent1 = new AllstarFighterDataWrapper(this, dataPtr->_opponent1);
+            Opponent2 = new AllstarFighterDataWrapper(this, dataPtr->_opponent2);
+            Opponent3 = new AllstarFighterDataWrapper(this, dataPtr->_opponent3);
+            Opponent4 = new AllstarFighterDataWrapper(this, dataPtr->_opponent4);
+            Opponent5 = new AllstarFighterDataWrapper(this, dataPtr->_opponent5);
 
             return false;
         }
@@ -62,7 +155,17 @@ namespace BrawlLib.SSBB.ResourceNodes
         {
             // Copy the data back to the address
             AllstarStageTbl* dataPtr = (AllstarStageTbl*)address;
-            *dataPtr = _data;
+            dataPtr->_stage1 = _stage1;
+            dataPtr->_stage2 = _stage2;
+            dataPtr->_stage3 = _stage3;
+            dataPtr->_stage4 = _stage4;
+            dataPtr->_stage5 = _stage5;
+
+            dataPtr->_opponent1 = (AllstarFighterData)Opponent1;
+            dataPtr->_opponent2 = (AllstarFighterData)Opponent2;
+            dataPtr->_opponent3 = (AllstarFighterData)Opponent3;
+            dataPtr->_opponent4 = (AllstarFighterData)Opponent4;
+            dataPtr->_opponent5 = (AllstarFighterData)Opponent5;
         }
         public override int OnCalculateSize(bool force)
         {
