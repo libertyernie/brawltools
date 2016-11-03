708b5cd16ffe7b956f7ba722c8faaae38d9ff197
libertyernie <maybeway36@gmail.com>
2016-11-03 13:37:47 -0500

    Throw a NotImplementedException if user tries to save a PCM16 .bcstm or .bfstm (not supported yet - I'd need to see some examples first)

--- BrawlLib/Wii/Audio/FSTMConverter.cs
+++ BrawlLib/Wii/Audio/FSTMConverter.cs
@@ -17,6 +17,9 @@ namespace BrawlLib.Wii.Audio
             StrmDataInfo strmDataInfo = *rstm->HEADData->Part1;
             int channels = strmDataInfo._format._channels;
 
+            if (strmDataInfo._format._encoding != (byte)WaveEncoding.ADPCM)
+                throw new NotImplementedException("FSTM export only supports ADPCM encoding.");
+
             // Get section sizes from the BRSTM - BFSTM is such a similar format that we can assume the sizes will match.
             int rstmSize = 0x40;
             int infoSize = rstm->_headLength;
