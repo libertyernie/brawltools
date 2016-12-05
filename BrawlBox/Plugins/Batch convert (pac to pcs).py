bcd5d2be35c3c17f74b2aea86984e9f988f44456
libertyernie <maybeway36@gmail.com>
2016-12-05 12:15:55 -0600

    Add a plugin script to batch convert pac to pcs (recursively) - see issue #92

---
+++ BrawlBox/Plugins/Batch convert (pac to pcs).py
@@ -0,0 +1,25 @@
+﻿from BrawlBox.API import bboxapi
+from BrawlLib.SSBB.ResourceNodes import *
+
+import os
+
+dir = bboxapi.OpenFolderDialog()
+if dir is not None:
+	from_ext = ".pac"
+	to_ext = ".pcs"
+
+	if not from_ext.startswith("."):
+		from_ext = "." + from_ext;
+		
+	if not to_ext.startswith("."):
+		to_ext = "." + to_ext;
+
+	for root, dirs, files in os.walk(dir):
+		for file in files:
+			if file.endswith(from_ext):
+				no_ext, ext = os.path.splitext(file)
+				node = NodeFactory.FromFile(None, os.path.join(root, file))
+				try:
+					node.Export(os.path.join(root, no_ext + to_ext))
+				except:
+					bboxapi.ShowMessage("Cannot export " + file + " as format " + to_ext, "Error")
