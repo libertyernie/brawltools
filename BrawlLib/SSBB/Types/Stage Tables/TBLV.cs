3298aeee04498eaa2a58beab065344f8229de97d
soopercool101 <soopercoolstages@gmail.com>
2018-04-21 12:28:46 -0700

    Removed unused variable from TBGM and TBLV

--- BrawlLib/SSBB/Types/Stage Tables/TBLV.cs
+++ BrawlLib/SSBB/Types/Stage Tables/TBLV.cs
@@ -12,7 +12,6 @@ namespace BrawlLib.SSBBTypes
         public bint _unk0;
         public bint _unk1;
         public bint _unk2;
-        //public bint _entryOffset;
 
         public VoidPtr Address { get { fixed (void* ptr = &this) return ptr; } }
         public bfloat* Entries { get { return (bfloat*)(Address + 0x10); } }
